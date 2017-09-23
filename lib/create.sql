229c0ccf9c0abe2798fef6eb0e0d1b85bd1c5857
rebeccanewborn <rjn58@cornell.edu>
2017-09-23 15:28:47 -0400

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,2 @@
+CREATE TABLE bears(id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender char(1),
+color TEXT, temperament TEXT, alive BOOLEAN);
---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate"
+, 30, 'M', "brown", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy"
+, 12, 'M', "black", "crazy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha"
+, 14, 'F', "black and brown", "nice", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown"
+, 38, 'M', "brown", "dominant", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa"
+, 19, 'F', "black", "sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch"
+, 47, 'M', "black", "grouchy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy"
+, 32, 'F', "brown", "calm", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL
+, 8, 'M', "black", "violent", 0);
